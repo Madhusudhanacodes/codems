ce5fa80a822277de66ff85956f9d5863854cb359
Madhusudhanacodes <dmadhusudhana.xworkz@gmail.com>
2024-07-25 13:56:45 +0530

    message

---
+++ git java/adhaar.java
@@ -0,0 +1,18 @@
+class adhaar{
+	public static void main(String[] args){
+	String adharnos1="LH1002";
+	String adharnos2="LH00001DGR";
+	String adharnos3="LH002920012";
+	String adharnos4="LK029912212";
+	String adharnos5="lk2000219330";
+	
+	String adharnos={adharnos1,adharnos2,adharnos3,adharnos4,adharnos5};
+	int adharlen=adharnos.length;
+	System.out.println(adharlen);
+	System.out.println(adharnos[1]);
+	System.out.println(adharnos[2]);
+	System.out.println(adharnos[3]);
+	System.out.println(adharnos[4]);
+	System.out.println(adharnos[5]);
+	}	
+}
